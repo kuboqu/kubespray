f3c0193b23ca9c3d6f88299c29e06dbebd499be1
Bohdan <kuboqu@gmail.com>
2025-05-05 14:52:52 +0300

    Update node roles

--- contrib/terraform/aws/modules/iam/main.tf
+++ contrib/terraform/aws/modules/iam/main.tf
@@ -128,8 +128,61 @@ resource "aws_iam_role_policy" "kube-worker" {
 EOF
 }
 
-#Create AWS Instance Profiles
+# CSI Policy
+variable "csi_policy" {
+  default = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeAvailabilityZones",
+        "ec2:DescribeInstances",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DescribeVolumesModifications"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:CreateSnapshot",
+        "ec2:ModifyVolume"
+      ],
+      "Resource": "arn:aws:ec2:*:*:volume/*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:AttachVolume",
+        "ec2:DetachVolume"
+      ],
+      "Resource": [
+        "arn:aws:ec2:*:*:volume/*",
+        "arn:aws:ec2:*:*:instance/*"
+      ]
+    }
+  ]
+}
+EOF
+}
 
+resource "aws_iam_role_policy" "kube_control_plane_csi_user" {
+  name   = "kubernetes-${var.aws_cluster_name}-control-csi-user"
+  role   = aws_iam_role.kube_control_plane.id
+  policy = var.csi_policy
+}
+
+resource "aws_iam_role_policy" "kube-worker_csi_user" {
+  name   = "kubernetes-${var.aws_cluster_name}-node-csi-user"
+  role   = aws_iam_role.kube-worker.id
+  policy = var.csi_policy
+}
+
+#Create AWS Instance Profiles
 resource "aws_iam_instance_profile" "kube_control_plane" {
   name = "kube_${var.aws_cluster_name}_master_profile"
   role = aws_iam_role.kube_control_plane.name
--- contrib/terraform/aws/scripts/0-generate-inventory.sh
+++
@@ -1,3 +0,0 @@
-terraform output -json > output.json
-jinja2 templates/inventory.tpl output.json > inventory.ini
-# ansible-playbook -i inventory.ini cluster.yml -b -u ubuntu
